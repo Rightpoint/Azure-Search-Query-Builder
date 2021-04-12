d0dd8cf154ef8d13e2c5cc82eecaa2aecd559d43
Andrew Varnon <avarnon@users.noreply.github.com>
2021-04-12 07:34:57 -0400

    Upgrade to the new Search SDK and .NET 5

--- AzureSearchQueryBuilder/Builders/IOptionsBuilder.cs
+++ AzureSearchQueryBuilder/Builders/IOptionsBuilder.cs
@@ -21,11 +21,11 @@ namespace AzureSearchQueryBuilder.Builders
     public delegate bool BooleanLambdaDelegate<in TModel>(TModel model);
 
     /// <summary>
-    /// An interface representing an <see cref="ParametersBuilder"/>`2[<typeparamref name="TModel"/>, <typeparamref name="TParameters"/>] builder.
+    /// An interface representing an <see cref="OptionsBuilder"/>`2[<typeparamref name="TModel"/>, <typeparamref name="TOptions"/>] builder.
     /// </summary>
     /// <typeparam name="TModel">The type of the model representing the search index documents.</typeparam>
-    /// <typeparam name="TParameters">The type of the parameters object to be built.</typeparam>
-    public interface IParametersBuilder<TModel, TParameters>
+    /// <typeparam name="TOptions">The type of the Options object to be built.</typeparam>
+    public interface IOptionsBuilder<TModel, TOptions>
     {
         /// <summary>
         /// Gets the expression that filters the documents considered for producing the completed term suggestions.
@@ -55,41 +55,41 @@ namespace AzureSearchQueryBuilder.Builders
         /// <summary>
         /// Gets the number of items to retrieve.
         /// </summary>
-        int? Top { get; }
+        int? Size { get; }
 
         /// <summary>
-        /// Build a <typeparamref name="TParameters"/> object.
+        /// Build a <typeparamref name="TOptions"/> object.
         /// </summary>
-        /// <returns>the <typeparamref name="TParameters"/> object.</returns>
-        TParameters Build();
+        /// <returns>the <typeparamref name="TOptions"/> object.</returns>
+        TOptions Build();
 
         /// <summary>
         /// Adds a where clause to the filter expression.
         /// </summary>
         /// <param name="lambdaExpression">The lambda expression used to generate a filter expression.</param>
         /// <returns>the updated builder.</returns>
-        IParametersBuilder<TModel, TParameters> Where(Expression<BooleanLambdaDelegate<TModel>> lambdaExpression);
+        IOptionsBuilder<TModel, TOptions> Where(Expression<BooleanLambdaDelegate<TModel>> lambdaExpression);
 
         /// <summary>
         /// Sets the string tag that appends to search hits.
         /// </summary>
         /// <param name="highlightPostTag">the desired tag.</param>
         /// <returns>the updated builder.</returns>
-        IParametersBuilder<TModel, TParameters> WithHighlightPostTag(string highlightPostTag);
+        IOptionsBuilder<TModel, TOptions> WithHighlightPostTag(string highlightPostTag);
 
         /// <summary>
         /// Sets the string tag that prepends to search hits.
         /// </summary>
         /// <param name="highlightPreTag">the desired tag.</param>
         /// <returns>the updated builder.</returns>
-        IParametersBuilder<TModel, TParameters> WithHighlightPreTag(string highlightPreTag);
+        IOptionsBuilder<TModel, TOptions> WithHighlightPreTag(string highlightPreTag);
 
         /// <summary>
         /// sets a number between 0 and 100 indicating the percentage of the index that must be covered by a query in order for the query to be reported as a success. 
         /// </summary>
         /// <param name="minimumCoverage">The desired minimum coverage.</param>
         /// <returns>the updated builder.</returns>
-        IParametersBuilder<TModel, TParameters> WithMinimumCoverage(double? minimumCoverage);
+        IOptionsBuilder<TModel, TOptions> WithMinimumCoverage(double? minimumCoverage);
 
         /// <summary>
         /// Appends to the list of field names to search for the specified search text.
@@ -97,13 +97,13 @@ namespace AzureSearchQueryBuilder.Builders
         /// <typeparam name="TProperty"></typeparam>
         /// <param name="lambdaExpression">The lambda expression representing the search field.</param>
         /// <returns>the updated builder.</returns>
-        IParametersBuilder<TModel, TParameters> WithSearchField<TProperty>(Expression<PropertyLambdaDelegate<TModel, TProperty>> lambdaExpression);
+        IOptionsBuilder<TModel, TOptions> WithSearchField<TProperty>(Expression<PropertyLambdaDelegate<TModel, TProperty>> lambdaExpression);
 
         /// <summary>
         /// Sets the number of items to retrieve. 
         /// </summary>
         /// <param name="top">The desired top value.</param>
         /// <returns>the updated builder.</returns>
-        IParametersBuilder<TModel, TParameters> WithTop(int? top);
+        IOptionsBuilder<TModel, TOptions> WithSize(int? size);
     }
 }
